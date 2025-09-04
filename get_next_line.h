1aa6061562c7a5c8138db6ec4266118e21cfc44a
gabrgarc <gabrgarc@student.42sp.org.br>
2025-09-04 17:48:45 -0300

    first header for GNL

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 11:39:03 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/09/03 11:05:33 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	char			*content;
+	int				i;
+	ssize_t			len;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+
+#endif
