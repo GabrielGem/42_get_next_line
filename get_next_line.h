9d81fe4b5285dd3d3f5b74358b0fd41a9418d760
gabrgarc <gabrgarc@student.42sp.org.br>
2025-09-16 20:37:32 -0300

    add function last_node in header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 11:39:03 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/09/14 18:53:37 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/09/16 20:15:30 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,11 @@ typedef struct s_list
 
 char	*get_next_line(int fd);
 int		get_hunk_line(t_list **head, int fd);
-ssize_t	get_info_node(t_list **node);
-char	*get_line(t_list **head, char **line);
+int		get_info_node(t_list **node);
+char	*get_line(t_list **head, char **line, int i_line);
 t_list	*check_remain(t_list **head);
 t_list	*init_node(void);
+t_list	*last_node(t_list **head);
 void	*free_list(t_list **head);
 
 #endif
